19196fd12af56e73523319ba1204989492547107
navjc <jcnavarro@calvia.com>
2024-09-04 14:40:17 +0200

    Add files via upload

---
+++ USERS_NO_CADUCAN.ps1
@@ -0,0 +1,13 @@
+﻿# Ejecutar como administrador
+# Deshabilitar el caducamiento de contraseñas para todos los usuarios
+
+# Obtener todos los usuarios locales
+$users = Get-LocalUser
+
+foreach ($user in $users) {
+    # Cambiar la configuración de caducidad de contraseña a "nunca expira"
+    Set-LocalUser -Name $user.Name -PasswordNeverExpires $true
+    Write-Host "La contraseña para el usuario $($user.Name) ahora nunca caducará."
+}
+
+Write-Host "Se ha deshabilitado el caducamiento de contraseñas para todos los usuarios locales."
